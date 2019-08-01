56cdca0be60a9d238f1a0edef681ca7e29b11edd
manishgurungxon <manishgurung.on@gmail.com>
2019-07-31 22:04:40 -0500

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,60 +1,73 @@
 module.exports = {
   profile: {
-    name: 'Irfan Maulana',
-    title: 'Principal Engineer for Frontend at Tokopedia',
-    photo: 'https://avatars2.githubusercontent.com/u/7221389?s=300&v=4', // 300x300px
-    city: 'Jakarta, Indonesia',
-    phone: '(+62) 857-8123-8798',
-    mail: 'mazipanneh@gmail.com'
+    name: 'Manish Gurung',
+    title: 'Fresh Graduate Student',
+    photo: 'https://avatars1.githubusercontent.com/u/26619307?s=460&v=4', // 300x300px
+    city: 'West Bend, Wisconsin',
+    phone: '(262)-323-1474',
+    mail: 'la1000aero@gmail.com'
   },
   social: {
     facebook: {
-      name: 'mazipanneh',
-      link: 'https://web.facebook.com/mazipanneh'
+      name: 'manish gurung',
+      link: 'https://web.facebook.com/manishgurung.on'
     },
     twitter: {
-      name: '@maz_ipan',
-      link: 'https://twitter.com/Maz_Ipan'
+      name: '@kayen101',
+      link: 'https://twitter.com/la1000aero'
     },
     linkedin: {
-      name: 'in/mazipan',
-      link: 'https://www.linkedin.com/in/mazipan'
+      name: 'in/la1000aero',
+      link: 'https://www.linkedin.com/in/la1000aero'
     },
     github: {
-      name: 'mazipan',
-      link: 'https://github.com/mazipan'
+      name: 'manish gurung',
+      link: 'https://github.com/manishgurungxon'
     }
   },
-  summary: `An experienced software engineer especially in frontend side.<br/>Having deep knowledge in HTML, CSS (with its pre-processor) and JavaScript with the various framework. Write good quality, well tested, and fast delivered code.<br/>Experience in architecting frontend codebase.<br/>A well-known figure in the programming community, open-source creator, and writer for many technical articles.`,
+  summary: `I am a fresher I don't have industrial experience but myself working on C, C++, Data Structure, Python<br/>Having a basic knowledge in HTML, CSS and JavaScript with the various framework. Create good quality contents, well tested, and fast delivered.<br/>Knowledge in Big Data and data science.`,
   experiences: [
     {
-      company: 'Tokopedia',
-      location: 'Jakarta',
-      title: 'Principal Engineer for Frontend',
-      period: 'JUN 2019 - PRESENT',
-      jobdesc: [],
-    },
-    {
-      company: 'Bizzy Indonesia',
-      location: 'Jakarta',
-      title: 'Software Architect for Frontend',
-      period: 'FEB 2019 - JUN 2019',
+      company: 'Walmart',
+      location: '1515 W Paradise Dr, West Bend, WI 53095, United States',
+      title: 'Electronic Sales Associate',
+      period: 'October 2017 - 2019',
       jobdesc: [
-        'Split monolith frontend codebase into some different repository',
-        'Introduce many best practices in coding Vue.js and Nuxt.js',
-        'Creating many common utilities for easier sharing code that managed in monorepo using Lerna',
-        'Monitoring web performance',
-        'Actively sharing about how to be effective, good quality, and excellent software development process especially in frontend side.'
+      'Maintained the electronic area in accordance with company policies and procedures by properly handling claims, returns, zoning the area, arranging and organizing merchandise/supplies. Identified shrink and damages, and ensured a safe work environment. ',
+      'Maintained merchandise presentation in the electronic area by stocking and rotating merchandise. ',
+      'Provided excellent customer service by acknowledging the customer, identifying customer needs, assisting with purchasing decisions, locating merchandise, resolving customer issues and concerns, and promoting products and services, while maintaining a safe shopping environment. ',
+      'Received stocks merchandise/supplies from distribution centers and suppliers and organizes and maintained the electronics are by following company procedures, completing and retaining paperwork, logs and other documentation. ',
+      'Operate equipment, such as cash registers and related tools, to process customer purchases using appropriate procedures for different payment types and items sold. ',
+      'Organized and maintained the electronics department by securing merchandise, assisting customers with warranty transactions, maintaining the operation of displays, and categorizing videos, CDs and DVDs. ',
       ],
     },
     {
-      company: 'Bizzy Indonesia',
-      location: 'Jakarta',
-      title: 'Software Development Engineer',
-      period: 'JUL 2018 - FEB 2019',
+      company: 'Pagoda Labs Pvt ltd',
+      website: 'www.pagodalabs.com',
+      location: 'Sanepa, Kathmandu',
+      title: 'Digital Marketer',
+      period: 'JUN 2015 - AUG 2016',
       jobdesc: [
-        'Helping small team in transaction process to develop new features and fixing bugs',
-        'Setup and spread about unit test adoption in frontend codebase'
+        'Created a yearlong digital campaign project and presented to banking agencies and MNCs. ',
+        'Fulfilled the role of social media page admin for brands Facebook page, Twitter, Instagram and Social Media and helped boost organic likes with creative contents. ',
+        'Generated organic likes and views on social media pages and also facilaited for paid likes and views in social web pages such as facebook, instagram and youtube.',
+        'Monitoring social media page performance.',
+        'Using Social media tools such as Hootsuite to collect and gather metrics on contents engagement and awareness outputs.'
+        'fulfilled the duties of data extraction, data filteration and data visualization from customers engagement through social media pages.'
+      ],
+    },
+    {
+      company: 'Youth For Human Rights Nepal',
+      website: 'www.yhrnepal.org',
+      location: 'BhagwatiMarg, Naxal, Kathmandu, Nepal',
+      title: 'Board Member',
+      period: 'JUL 2012 - FEB 2017',
+      jobdesc: [
+        'Helping maintain social media pages and website with contents',
+        'Managing events like “International Human Rights Walk” every year on 8th December.',
+        'Provide booklets in schools and colleges which are located in rural parts of Nepal.',
+        'Workshops in private schools in Kathmandu city.', 
+        'Created International Youth Forum on Human Rights and United Nations Sustainable Development Goals 2017 held in Kathmandu, Nepal.',
       ],
     },
     {
@@ -68,74 +81,101 @@ module.exports = {
       ],
     },
     {
-      company: 'Blibli.com',
-      location: 'Jakarta',
-      title: 'Software Development Engineer',
-      period: 'OCT 2015 - JAN 2017',
+      company: 'Ocean Computer|MSI INTERNATIONAL ',
+      location: 'Kathmandu, Nepal',
+      title: 'Sales Agent Internship',
+      period: 'May 2012 - Oct 2012',
       jobdesc: [
-        'Maintenance, bug fixing, adding new feature and improvement in our website.',
-        'Doing frontend stuff with AngularJS, jQuery, Vanilla Javascript, JSP, JSTL, SASS, CSS3.',
-        'Doing small stuff in backend using SpringMVC.'
+        'Building strong relation with retailers using B2B strategy ',
+        'Providing creative and strong CRM approaches for branding',
+        'Informing about product changes and product branding to retailers (B2B)',
+        'Created and stored data collected on MSI gaming laptops and provided strategical ways to boost sales and customer awareness in Nepal.',
       ],
     },
     {
-      company: 'SML Technologies',
-      location: 'Jakarta',
-      title: 'Java Developer',
-      period: 'AUG 2013 - OCT 2015',
+      company: 'SMD Co-operatives Pvt ltd.',
+      location: 'Lalitpur, Nepal',
+      title: 'Office Accountant',
+      period: 'AUG 2009 - OCT 2010',
       jobdesc: [
-        'Develop java desktop application with JIDE and JDAF Framework',
-        'Develop front-end web application with Sencha ExtJs Framework.',
-        'Develop back-end Rest API with EJB and JPA.'
+        'Fulfilling every day duties of book-keeping and closing of transactions in data.',
+        'Managing daily ledgers and journals. ',
+        'Customer service to clients and customers.',
+        'Filing in and out applications for registrations.', 
+        'Overseeing accounts payables and accounts receivables.Preparing financial reports for the firm.', 
+        'Preparing financial reports or balance sheets.',
       ],
     }
   ],
   educations: [
     {
-      name: 'Muhammadiyah University',
-      city: 'Jakarta',
-      degree: 'Bachelor Degree',
-      period: '2009 - 2013',
-      faculty: 'Bachelor of Informatics Engineering'
+      name: 'University of Wisconsin Milwaukee',
+      city: 'Milwaukee',
+      degree: 'Masters Degree',
+      period: '2019 - present',
+      faculty: 'Masters of Science in Information Science and Technology'
+    }
+   {
+      name: 'Queen Margaret University',
+      city: 'Edinburgh, UK',
+      degree: 'Bachelors Degree',
+      period: '2012-2017',
+      faculty: 'Bachelors in Business Administration'
+    }
+    {
+      name: 'University of People',
+      city: 'California',
+      degree: 'Bachelors Degree',
+      period: '2019 - present',
+      faculty: 'Bachelors of Science in Computer Science'
     }
   ],
   projects: [
     {
-      title: 'Bizzy Front-office and Back-office',
-      company: 'Bizzy Indonesia',
-      period: 'JUL 2018 - JUN 2019'
+      title: 'GetX',
+      company: 'Self project',
+      period: 'Current'
     },
     {
-      title: 'Blibli.com Travel, Blibli.com Pulsa, Multi-Merchant Feature',
-      company: 'Blibli.com',
-      period: 'OCT 2015 - JUN 2018'
+      title: 'Dreamstalk',
+      company: 'Self project',
+      period: 'Current'
     },
     {
-      title: 'Servo Analytica 4, Traversa R3 Routing Management, SIAGA Dashboard, Servo Availability Web, Dashboard Builder',
-      company: 'SML Technologies',
-      period: 'AUG 2013 - OCT 2015'
+      title: 'INTERNATIONAL YOUTH FORUM ON HUMAN RIGHTS',
+      company: 'Youth For Human Rights International',
+      year: '2017'
     }
   ],
   skills: [
     {
       name: 'Javascript',
-      progress: 70
+      progress: 40
     },
     {
       name: 'CSS',
-      progress: 70
+      progress: 50
     },
     {
       name: 'HTML',
       progress: 70
     },
     {
-      name: 'FE Development',
-      progress: 70
+      name: 'Python',
+      progress: 50
+    }
+     {
+      name: 'Java',
+      progress: 30
+    }
+     {
+      name: 'PHP',
+      progress: 30
     }
   ],
   languages: [
-    'Bahasa Indonesia',
-    'English'
+    'English',
+    'Nepali,',
+    'Hindi',
   ]
 }
